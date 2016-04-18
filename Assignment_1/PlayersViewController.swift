b424480c95ae7d104c627790134d519a2f820db6
Zakarion <computator_typeJ@Joshuas-Mac.local>
2016-04-17 19:30:56 -0700

    Finished Page 1 of Tut
    above

--- Assignment_1/PlayersViewController.swift
+++ Assignment_1/PlayersViewController.swift
@@ -43,20 +43,9 @@ class PlayersViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath)
         -> UITableViewCell {
             let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath:indexPath)
-            
+                as! PlayerCell
             let player = players[indexPath.row] as Player
-            
-            if let nameLabel = cell.viewWithTag(100) as? UILabel{
-                cell.textLabel?.text = player.name
-            }
-    
-            if let gameLabel = cell.viewWithTag(101) as? UILabel{
-                cell.detailTextLabel?.text = player.game
-            }
-    
-            if let ratingImageView = cell.viewWithTag(102) as? UIImageView{
-                ratingImageView.image = self.imageForRating(player.rating)
-            }
+            cell.player = player
             return cell
     }
     
@@ -105,18 +94,4 @@ class PlayersViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    
-    func imageForRating(rating:Int) -> UIImage?{
-        let imageName = "\(rating)Stars"
-        return UIImage(named: imageName)
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-
 }
